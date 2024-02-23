abd4968dbef13ba6fb0ca18cc311bcc1c4a5e3c1
Gabriel Basaglia <garbaroots@hotmail.com>
2024-02-23 20:50:22 -0300

    add container, ajuste sidebar e logica

--- src/app/components/hero-section/index.jsx
+++ src/app/components/hero-section/index.jsx
@@ -2,6 +2,7 @@
 import bgmain from "@/assets/bg-main.png";
 import Image from "next/image";
 import { Techno } from "../techno";
+import { Container } from "../container";
 
 export default function HeroSection(toggleSidebar) {
   return (
@@ -9,27 +10,29 @@ export default function HeroSection(toggleSidebar) {
       <Image
         src={bgmain}
         alt="Background"
-        className="absolute bg-fixed bg-cover w-screen h-screen  opacity-5"
+        className="absolute top-0 left-0 w-full h-full bg-fixed bg-cover opacity-10 blur-sm "
       />
 
-      <section className="mx-auto mt-20 relative z-10 ">
-        <div className="md:p-0 md:ml-96 md:mr-16 w-screen px-5">
-          <h3 className="text-textPrimary text-lg ">Olá, tudo bem por aí?</h3>
-          <h1 className="text-5xl text-textPrimary">
-            Eu sou o{" "}
-            <span className="text-textSecondary">Gabriel Basaglia</span>
-          </h1>
-          <h2 className="text-textPrimary text-2xl mt-4">
-            Desenvolvedor FullStack
-          </h2>
-          <p className="text-textPrimary text-md mt-4 lg:mr-72 mb-14 md:w-1/2">
-            Meu foco está em contribuir para soluções inovadoras, aprender
-            continuamente e colaborar em projetos que desafiem minhas
-            habilidades.
-          </p>
-        </div>
+      <Container>
+        <section className="mx-auto z-10 w-full ">
+          <div className="md:p-0 md:ml-96 md:mr-16 px-5">
+            <h3 className="text-textPrimary text-lg ">Olá, tudo bem por aí?</h3>
+            <h1 className="text-5xl text-textPrimary">
+              Eu sou o{" "}
+              <span className="text-textSecondary">Gabriel Basaglia</span>
+            </h1>
+            <h2 className="text-textPrimary text-2xl mt-4">
+              Desenvolvedor FullStack
+            </h2>
+            <p className="text-textPrimary text-md mt-4 lg:mr-72 mb-14 md:w-1/2">
+              Meu foco está em contribuir para soluções inovadoras, aprender
+              continuamente e colaborar em projetos que desafiem minhas
+              habilidades.
+            </p>
+          </div>
+        </section>
         <Techno />
-      </section>
+      </Container>
     </main>
   );
 }
--- src/app/components/techno/index.jsx
+++ src/app/components/techno/index.jsx
@@ -2,81 +2,41 @@ import { FaTools, FaReact, FaNodeJs, FaPython } from "react-icons/fa";
 import { TbBrandNextjs } from "react-icons/tb";
 import { SiMongodb, SiAstro, SiTailwindcss } from "react-icons/si";
 
+const technologies = [
+  { icon: <FaReact size={56} />, name: "React" },
+  { icon: <FaNodeJs size={56} />, name: "Nodejs" },
+  { icon: <FaPython size={56} />, name: "Python" },
+  { icon: <TbBrandNextjs size={56} />, name: "Nextjs" },
+  { icon: <SiMongodb size={56} />, name: "Mongodb" },
+  { icon: <SiAstro size={56} />, name: "Astro" },
+  { icon: <SiTailwindcss size={56} />, name: "Tailwindcss" },
+];
+
 export function Techno() {
   return (
-    <section className="md:ml-96">
-      <div className="flex items-center gap-2 text-textPrimary px-5 md:p-0">
-        <h1 className="font-semibold text-2xl ">Tecnologias</h1>
+    <section className="">
+      <div className="flex items-center gap-2 text-textPrimary px-5 md:p-0 md:ml-96">
+        <h1 className="font-semibold text-2xl">Tecnologias</h1>
         <FaTools />
         <h1 className="font-semibold text-2xl">Ferramentas</h1>
       </div>
 
-      <div className="border-y-2 mt-4 border-slate-500 flex md:gap-20 md:-ml-[85px] whitespace-nowrap overflow-hidden">
-        <div className=" animate-slide flex gap-4 md:gap-20">
-          <div className="">
-            <ul className="flex my-4 gap-4 md:gap-20 ">
-              <li className="flex gap-4 items-center">
-                <FaReact size={56} />
-                <p className="text-xl">React</p>
-              </li>
-              <li className="flex gap-4 items-center">
-                <FaNodeJs size={56} />
-                <p className="text-xl">Nodejs</p>
-              </li>
-              <li className="flex gap-4 items-center">
-                <FaPython size={56} />
-                <p className="text-xl">Python</p>
-              </li>
-              <li className="flex gap-4 items-center">
-                <TbBrandNextjs size={56} />
-                <p className="text-xl">Nextjs</p>
-              </li>
-              <li className="flex gap-4 items-center">
-                <SiMongodb size={56} />
-                <p className="text-xl">Mongodb</p>
-              </li>
-              <li className="flex gap-4 items-center">
-                <SiAstro size={56} />
-                <p className="text-xl">Astro</p>
-              </li>
-              <li className="flex gap-4 items-center">
-                <SiTailwindcss size={56} />
-                <p className="text-xl">Tailwindcss</p>
-              </li>
-            </ul>
-          </div>
-          <div className="">
-            <ul className="flex my-4 gap-4 md:gap-20 ">
-              <li className="flex gap-4 items-center">
-                <FaReact size={56} />
-                <p className="text-xl">React</p>
-              </li>
-              <li className="flex gap-4 items-center">
-                <FaNodeJs size={56} />
-                <p className="text-xl">Nodejs</p>
-              </li>
-              <li className="flex gap-4 items-center">
-                <FaPython size={56} />
-                <p className="text-xl">Python</p>
-              </li>
-              <li className="flex gap-4 items-center">
-                <TbBrandNextjs size={56} />
-                <p className="text-xl">Nextjs</p>
-              </li>
-              <li className="flex gap-4 items-center">
-                <SiMongodb size={56} />
-                <p className="text-xl">Mongodb</p>
-              </li>
-              <li className="flex gap-4 items-center">
-                <SiAstro size={56} />
-                <p className="text-xl">Astro</p>
-              </li>
-              <li className="flex gap-4 items-center">
-                <SiTailwindcss size={56} />
-                <p className="text-xl">Tailwindcss</p>
-              </li>
-            </ul>
-          </div>
+      <div className="border-y-2 mt-4 border-slate-500 flex md:gap-20 whitespace-nowrap overflow-hidden ">
+        <div className="animate-slide flex gap-4 md:gap-20">
+          {Array(2)
+            .fill()
+            .map((_, index) => (
+              <div key={index}>
+                <ul className="flex my-4 gap-4 md:gap-20">
+                  {technologies.map((tech, techIndex) => (
+                    <li key={techIndex} className="flex gap-4 items-center">
+                      {tech.icon}
+                      <p className="text-xl">{tech.name}</p>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
         </div>
       </div>
     </section>
